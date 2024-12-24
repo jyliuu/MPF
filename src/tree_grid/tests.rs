53b412149388ccdf225e8d3004a024415aa207cb
Jinyang Liu <jl@math.ku.dk>
2024-12-24 19:52:50 +0100

    Test predict

--- src/tree_grid/tests.rs
+++ src/tree_grid/tests.rs
@@ -1,17 +1,21 @@
-#[cfg(test)]
 use super::*;
 
-#[test]
-fn test_tree_grid_refine_candidate() {
+fn setup_data() -> (Array2<f32>, Array1<f32>) {
     let x = Array2::from_shape_vec(
         (6, 2),
         vec![0.1, 0.2, 0.3, 0.5, 0.7, 0.6, 1.1, 1.2, 1.3, 1.5, 1.7, 1.6],
     )
     .unwrap();
+
     let y = Array1::from_vec(vec![0.5, 0.5, 0.5, 1.5, 1.5, 1.5]);
 
-    let tree_grid = TreeGrid::new(&x, &y);
+    (x, y)
+}
 
+#[test]
+fn test_tree_grid_refine_candidate() {
+    let (x, y) = setup_data();
+    let tree_grid = TreeGrid::new(&x, &y);
     let (_, _, refine_candidate) = tree_grid.slice_and_refine_candidate(0, 1.0);
     assert_eq!(refine_candidate.update_a, 0.5);
     assert_eq!(refine_candidate.update_b, 1.5);
@@ -19,13 +23,7 @@ fn test_tree_grid_refine_candidate() {
 
 #[test]
 fn test_tree_grid_slice_and_refine() {
-    let x = Array2::from_shape_vec(
-        (6, 2),
-        vec![0.1, 0.2, 0.3, 0.5, 0.7, 0.6, 1.1, 1.2, 1.3, 1.5, 1.7, 1.6],
-    )
-    .unwrap();
-    let y = Array1::from_vec(vec![0.5, 0.5, 0.5, 1.5, 1.5, 1.5]);
-
+    let (x, y) = setup_data();
     let mut tree_grid = TreeGrid::new(&x, &y);
     tree_grid.slice_and_refine(0, 1.0);
     assert_eq!(tree_grid.grid_values[0], vec![0.5, 1.5]);
@@ -35,4 +33,23 @@ fn test_tree_grid_slice_and_refine() {
         vec![(f32::NEG_INFINITY, 1.0), (1.0, f32::INFINITY)]
     );
     assert_eq!(tree_grid.residuals.sum(), 0.0);
+}
+
+#[test]
+fn test_tree_grid_fit_and_predict() {
+    let (x, y) = setup_data();
+    let mut tree_grid = TreeGrid::new(&x, &y);
+    tree_grid.slice_and_refine(0, 1.0);
+    tree_grid.slice_and_refine(1, 1.0);
+
+    let preds_on_x = tree_grid.predict(&x);
+    let preds_extrapolated = unsafe {
+        tree_grid.predict(&Array2::from_shape_vec_unchecked(
+            (2, 2),
+            vec![-1.0, -1.0, 2.0, 2.0],
+        ))
+    };
+
+    assert_eq!(preds_on_x, tree_grid.y_hat);
+    assert_eq!(preds_extrapolated, Array1::from_vec(vec![0.5, 1.5]));
 }
