7acafa42994e8956a4deb4a6f7e0c475d5d6396e
Jinyang Liu <jl@math.ku.dk>
2025-01-05 14:07:01 +0100

    Polish Python interface by deref wrapping

--- python/main.py
+++ python/main.py
@@ -7,11 +7,10 @@ x = np.random.randn(n, 2)
 y = 2 * x[:, 0] * x[:, 1]
 
 
-tg = mpf.RTGrid(n_iter = 100, split_try = 10, colsample_bytree = 1.0)
-tg.fit(x, y)
-
+tg = mpf.TreeGrid(n_iter = 100, split_try = 10, colsample_bytree = 1.0)
+fr = tg.fit(x, y)
+fr
 y_mean = np.mean(y)
-preds = tg.predict(x)
 
-print("Mean squared error: ", np.mean((preds - y) ** 2))
+print("Mean squared error: ", fr.err)
 print("Mean squared error (mean): ", np.mean((y_mean - y) ** 2))
--- src/tree_grid/model_py.rs
+++ src/tree_grid/model_py.rs
@@ -1,133 +1,107 @@
 use core::f64;
+use std::ops::{Deref, DerefMut};
 
-use super::{
-    fitter::{RefineCandidate, TreeGridFitter},
-    model::TreeGridParams,
-};
-use ndarray::{Array1, Axis};
+use super::model::{FitResult, TreeGrid, TreeGridParams};
 use numpy::{PyArray1, PyReadonlyArray1, PyReadonlyArray2, ToPyArray};
 use pyo3::prelude::*;
-use rand::prelude::*;
 
 #[derive(Debug)]
-#[pyclass(name = "RTGrid")]
-pub struct TreeGridPy {
-    is_fitted: bool,
-    splits: Vec<Vec<f64>>,
-    intervals: Vec<Vec<(f64, f64)>>,
-    grid_values: Vec<Vec<f64>>,
-    hyperparameters: TreeGridParams,
+#[pyclass(name = "TreeGrid")]
+pub struct TreeGridPy(TreeGrid);
+
+impl Deref for TreeGridPy {
+    type Target = TreeGrid;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for TreeGridPy {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
 }
 
 #[derive(Debug)]
-#[pyclass(name = "FResult")]
-pub struct FitResultPy {
-    #[pyo3(get)]
-    pub err: f64,
-    #[pyo3(get)]
-    pub residuals: Py<PyArray1<f64>>,
-    #[pyo3(get)]
-    pub y_hat: Py<PyArray1<f64>>,
+#[pyclass(name = "FitResult")]
+pub struct FitResultPy(FitResult);
+
+impl Deref for FitResultPy {
+    type Target = FitResult;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for FitResultPy {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+#[pymethods]
+impl FitResultPy {
+    #[getter]
+    pub fn get_err(&self) -> f64 {
+        self.err
+    }
+
+    #[getter]
+    pub fn get_residuals<'py>(&self, py: Python<'py>) -> PyResult<Bound<'py, PyArray1<f64>>> {
+        Ok(self.residuals.to_pyarray(py))
+    }
+
+    #[getter]
+    pub fn get_y_hat<'py>(&self, py: Python<'py>) -> PyResult<Bound<'py, PyArray1<f64>>> {
+        Ok(self.y_hat.to_pyarray(py))
+    }
+
+    fn __repr__(&self) -> String {
+        format!(
+            "FitResult(err={}, residuals={}, y_hat={})",
+            self.err, self.residuals, self.y_hat
+        )
+    }
 }
 
 #[pymethods]
 impl TreeGridPy {
     #[new]
     fn new(n_iter: usize, split_try: usize, colsample_bytree: f64) -> Self {
-        let splits = vec![];
-        let intervals = vec![];
-        let grid_values = vec![];
-
-        TreeGridPy {
-            is_fitted: false,
-            splits,
-            intervals,
-            grid_values,
-            hyperparameters: TreeGridParams {
-                n_iter,
-                split_try,
-                colsample_bytree,
-            },
-        }
+        TreeGridPy(TreeGrid::new(TreeGridParams {
+            n_iter,
+            split_try,
+            colsample_bytree,
+        }))
     }
 
-    pub fn predict<'py>(
+    #[getter]
+    pub fn get_fitted(&self) -> PyResult<bool> {
+        Ok(self.is_fitted)
+    }
+
+    #[pyo3(name = "predict")]
+    pub fn _predict<'py>(
         &self,
         py: Python<'py>,
         x: PyReadonlyArray2<'py, f64>,
     ) -> PyResult<Bound<'py, PyArray1<f64>>> {
         let x = x.as_array();
-        let mut y_hat = Array1::zeros(x.nrows());
-        for (i, row) in x.axis_iter(Axis(0)).enumerate() {
-            let mut prod = 1.0;
-            for (j, &val) in row.iter().enumerate() {
-                let index = self.splits[j]
-                    .iter()
-                    .position(|&x| val < x)
-                    .unwrap_or(self.splits[j].len());
-                prod *= self.grid_values[j][index];
-            }
-            y_hat[i] = prod;
-        }
+        let y_hat = self.predict(x);
         Ok(y_hat.to_pyarray(py))
     }
 
-    pub fn fit<'py>(
+    #[pyo3(name = "fit")]
+    pub fn _fit<'py>(
         &mut self,
-        py: Python<'py>,
         x: PyReadonlyArray2<'py, f64>,
         y: PyReadonlyArray1<'py, f64>,
     ) -> PyResult<FitResultPy> {
         let x = x.as_array();
         let y = y.as_array();
-        let mut fitter = TreeGridFitter::new(x, y);
-        let mean_err = fitter.residuals.pow2().mean().unwrap();
-        let mut rng = thread_rng();
-
-        for _ in 0..self.hyperparameters.n_iter {
-            // sample random columns to split on
-            let n_cols_to_sample =
-                (self.hyperparameters.colsample_bytree * x.ncols() as f64) as usize;
-
-            let split_idx: Vec<usize> = (0..self.hyperparameters.split_try)
-                .map(|_| rng.gen_range(0..x.nrows()))
-                .collect();
-
-            let mut possible_indices: Vec<usize> = (0..x.ncols()).collect();
-            possible_indices.shuffle(&mut rng);
-
-            let col_idx = possible_indices[0..n_cols_to_sample].to_vec();
-
-            let mut best_candidate: Option<RefineCandidate> = None;
-            let mut best_err_diff = f64::NEG_INFINITY;
-            for col in &col_idx {
-                for idx in &split_idx {
-                    let split = x[[*idx, *col]];
-                    let (err_new, err_old, refine_candidate) =
-                        fitter.slice_and_refine_candidate(*col, split);
-
-                    let err_diff = err_old - err_new;
-                    if err_diff > best_err_diff {
-                        best_candidate = Some(refine_candidate);
-                        best_err_diff = err_diff;
-                    }
-                }
-            }
-
-            if let Some(update_candidate) = best_candidate {
-                fitter.update_tree(update_candidate);
-            }
-        }
-
-        let err = fitter.residuals.pow2().mean().unwrap();
-        self.splits = fitter.splits;
-        self.intervals = fitter.intervals;
-        self.grid_values = fitter.grid_values;
-        self.is_fitted = true;
-        Ok(FitResultPy {
-            err,
-            residuals: fitter.residuals.to_pyarray(py).unbind(),
-            y_hat: fitter.y_hat.to_pyarray(py).unbind(),
-        })
+        let fit_result = self.fit(x, y);
+        Ok(FitResultPy(fit_result))
     }
 }
