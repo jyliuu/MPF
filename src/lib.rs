2954541c3e49388c5b4442d0d4e2f42e39d3ca7c
Jinyang Liu <jl@math.ku.dk>
2025-01-06 17:14:44 +0100

    Remove old template code

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,10 @@
 pub mod forest;
 pub mod tree_grid;
+use ndarray::Array1;
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
-/// A Python module implemented in Rust. The name of this function must match
-/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
-/// import the module.
 #[pymodule]
 fn mpf(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?);
     m.add_class::<tree_grid::py_bindings::TreeGridPy>()?;
     m.add_class::<tree_grid::py_bindings::FitResultPy>()?;
     Ok(())
