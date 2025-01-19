b791a53f8360e3f1fc9622707c63196e8dd07e6d
Jinyang Liu <jl@math.ku.dk>
2025-01-19 11:35:32 +0100

    Add abstractions for model fitting

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,24 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
 pub mod forest;
 pub mod tree_grid;
-use ndarray::Array1;
+use ndarray::{Array1, ArrayView2};
 
 #[derive(Debug)]
 pub struct FitResult {
     pub err: f64,
     pub residuals: Array1<f64>,
     pub y_hat: Array1<f64>,
+}
+
+pub trait FittedModel {
+    fn predict(&self, x: ArrayView2<f64>) -> Array1<f64>;
+}
+
+pub trait ModelFitter {
+    type Model: FittedModel;
+    type HyperParameters;
+
+    fn new(x: ArrayView2<f64>, y: ArrayView2<f64>) -> Self;
+    fn fit(&self, hyperparameters: Self::HyperParameters) -> (FitResult, Self::Model);
 }
